6091e57262950f472b4cbd905b3530a05c6868b5
rubyliao <rubyliao@tencent.com>
2019-09-06 14:53:31 +0800

    修改pom文件结构

--- src/main/java/com/github/monkeywie/GoMain.java
+++ src/main/java/com/github/monkeywie/GoMain.java
@@ -1,11 +1,17 @@
 package com.github.monkeywie;
 
+import com.github.monkeywie.proxyee.handler.HttpProxyServerHandle;
 import com.github.monkeywie.proxyee.server.HttpProxyServer;
 import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class Main {
+public class GoMain {
+    private static final Logger logger = LoggerFactory.getLogger(HttpProxyServerHandle.class);
 
     public static void main(String[] args) throws Exception {
+
+        logger.info("server begin");
         HttpProxyServerConfig config =  new HttpProxyServerConfig();
         config.setHandleSsl(true);
         new HttpProxyServer()
