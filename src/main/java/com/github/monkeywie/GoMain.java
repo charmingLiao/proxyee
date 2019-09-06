7edf5cdbe883192d7838e87dec40ea3fa08039eb
rubyliao <rubyliao@tencent.com>
2019-09-06 15:36:27 +0800

    修改pom文件结构

--- src/main/java/com/github/monkeywie/GoMain.java
+++ src/main/java/com/github/monkeywie/GoMain.java
@@ -6,6 +6,11 @@ import com.github.monkeywie.proxyee.server.HttpProxyServerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/*
+测试命令行
+curl -k "/data/git_proj/proxyee/src/main/resources/ca.crt"  -x "9.134.19.211:9911"  "https://play.google.com/store/apps/collection/topselling_free?s
+curl -k D:\Git_proj\github-open\proxyee\src\main\resources\ca.crt -x "9.134.19.211:9911"  https://www.baidu.com/ -v
+* */
 public class GoMain {
     private static final Logger logger = LoggerFactory.getLogger(HttpProxyServerHandle.class);
 
